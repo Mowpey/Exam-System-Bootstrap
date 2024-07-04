3e3afe2e6fe2c1c0bd763bbbd0ba4d0243f44bc8
Dawntless <66586346+Mowpey@users.noreply.github.com>
2024-07-04 16:51:33 +0800

    table-with-filters
    Preliminary filter

---
+++ main-script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const statusCells = document.querySelectorAll(
+    ".table.table-hover tbody tr td:nth-last-of-type(4)"
+  );
+
+  statusCells.forEach((statusCell) => {
+    const status = statusCell.textContent.trim();
+
+    if (status === "Passed") {
+      statusCell.classList.add("table-success");
+    } else if (status === "Failed") {
+      statusCell.classList.add("table-danger");
+    } else if (status === "Pending") {
+      statusCell.classList.add("table-info");
+    }
+  });
+});
